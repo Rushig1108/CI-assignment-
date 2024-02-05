6da6fbc8ede403c5964061a2a80987e8d41fe9e8
Rushikesh <124681936+Rushig1108@users.noreply.github.com>
2024-02-05 08:21:59 -0500

    Updated with Unit testing

--- src/main/java/com/example/DemoApp.java
+++ src/main/java/com/example/DemoApp.java
@@ -1,7 +1,11 @@
 package com.example;
 
 public class DemoApp {
+    public static String getGreeting() {
+        return "Hello, Jenkins and Maven!";
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello, Jenkins and Maven!");
+        System.out.println(getGreeting());
     }
 }
